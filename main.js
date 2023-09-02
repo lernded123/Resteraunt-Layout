4ebf8455ec5461b6933f23ba221e1c5e9ed6578a
Sams <goodlifesam@gmail.com>
2023-09-01 23:04:35 -0700

    JS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelector("[data-carousel-button]");
+const buttons = document.querySelectorAll("[data-carousel-button]");
 
 buttons.forEach((button) => {
   button.addEventListener;
