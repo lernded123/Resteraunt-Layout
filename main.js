16f198e8f31e52e4ef95605788e10aeb6f986a15
Sams <goodlifesam@gmail.com>
2023-09-02 17:26:46 -0700

    typo fix

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ buttons.forEach((button) => {
 
     const activeSlide = slides.querySelector("[data-active]");
     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if(newIndex < 0) newIndex = slides.children.length - 1
-    if(newIndex > = slides.children.length)
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
   });
 });
