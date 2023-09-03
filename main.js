d1f5c9894ed9211030568e0d0f6d84700107a546
Sams <goodlifesam@gmail.com>
2023-09-02 17:13:42 -0700

    added js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button.closest("[data-carousel]");
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
   });
 });
